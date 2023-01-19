158060d8f2a6326de6d28d1ae9cbe7b963c0216c
Ahammed Fahim Galib <galib0124gmail.com>
2023-01-19 17:54:45 +0600

    sigle category complete

--- src/Components/Home/AllCategories/SingleCategory.js
+++ src/Components/Home/AllCategories/SingleCategory.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SingleCategory = ({single}) => {
-    const {image, name} =  single.show
+    const {image, name, rating, id} =  single.show
     const {original} = image;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl my-10">
-            <figure><img className='h-96 w-full' src={original} alt="Shoes" /></figure>
+            <Link><figure className='cursor-pointer'><img className='max-h-[500px] w-full' src={original} alt="Shoes" /></figure></Link>
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                <button className="btn btn-primary">Buy Now</button>
+                <div>
+                    <h2 className="card-title text-white">{name}</h2>
+                    <p className='text-white'>Rating : {rating.average ? rating.average : '0'}</p>
                 </div>
             </div>
         </div>
--- src/Components/Home/HeroArea/HeroArea.js
+++ src/Components/Home/HeroArea/HeroArea.js
@@ -6,11 +6,11 @@ const HeroArea = () => {
             <div id="slide1" className="carousel-item relative w-full">
                 <div className="hero min-h-screen" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1487174244970-cd18784bb4a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80")` }}>
                     <div className="hero-overlay bg-opacity-60"></div>
-                    <div className="hero-content text-center text-neutral-content">
+                    <div className="hero-content text-left text-neutral-content">
                         <div className="max-w-md">
                         <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
                         <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                            <button className="btn btn-primary">Get Started</button>
                         </div>
                     </div>
                 </div>
